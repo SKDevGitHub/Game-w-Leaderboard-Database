0ea23c0fafce0463e7a51a825bf15f86e4a0063a
sgkkpf <sgkkpf@umsystem.edu>
2024-11-19 14:19:19 -0600

    ITS A BIG ONE FOLKS

--- landingpage.py
+++ landingpage.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import server_request_handler as server
 import display_level as dl
 import user_page as du
@@ -6,9 +7,8 @@ import user_page as du
 FONT = ("Verdana", 12)
 def displayLanding(username):
     class landingPage(tk.Frame):
-        def __init__(self, parent, controller):
+        def __init__(self, parent):
             tk.Frame.__init__(self, parent)
-            self.controller = controller
             label = tk.Label(self, text="Landing Page", font=("Verdana", 16, "bold"))
             label.pack(pady=20)
 
@@ -18,10 +18,10 @@ def displayLanding(username):
             self.search_level = tk.Entry(self, font=FONT)
             self.search_level.pack(pady=(5,10))
 
-            search_lbutton = tk.Button(self, text="Search User", command=self.searchLevel)
+            search_lbutton = tk.Button(self, text="Search Level", command=self.searchLevel)
             search_lbutton.pack(pady=(0,20))
 
-            userLabel = tk.Label(self, text = "Search Level", font = FONT)
+            userLabel = tk.Label(self, text = "Search User", font = FONT)
             userLabel.pack(pady=(10,0))
             
             self.search_user = tk.Entry(self, font=FONT)
@@ -32,25 +32,27 @@ def displayLanding(username):
         
         def searchLevel(self):
             levelName = self.search_level.get()
-            self.controller(levelName)
+            search_level(levelName)
         
         def searchUser(self): 
             userName = self.search_user.get()
-            self.controller(userName)
+            search_user(userName)
 
-        def search_level(page_name):
+    def search_level(page_name):
             check = server.search_for_level(page_name)
             if len(check) != 0:
+                print(f"CHECKER LEVEL: {check[0]}")
                 dl.display_level(check[0], username)
             else:
-                tk.messagebox.showinfo("Level Not Found")
+                messagebox.showinfo("Level Not Found")
     
-        def search_user(page_name):
+    def search_user(page_name):
             check = server.search_for_user(page_name)
             if len(check) != 0:
+                print(f"CHECKER USERNAME: {check[0]}")
                 du.displayUser(check[0])
             else:
-                tk.messagebox.showinfo("User Not Found")
+                messagebox.showinfo("User Not Found")
 
     root = tk.Tk()
     root.title("Landing Page")
