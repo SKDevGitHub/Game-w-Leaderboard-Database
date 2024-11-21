da699b66221408f84dbfae030e37c6fc927c7275
eliot <eliotkimmelcentillion@gmail.com>
2024-11-21 15:31:43 -0600

    added install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+# This script requires you to have python installed and availible via the command line
+mkdir .venv
+python3 -m venv .venv
+pip3 install --upgrade pip
+pip3 install Levenshtein tk Pillow pygame
